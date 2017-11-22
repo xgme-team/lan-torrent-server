a26e43c2e86060ad88697b15c1b46eac5eacfb72
Johannes Spangenberg <johannes.spangenberg@hotmail.de>
2017-11-22 20:03:11 +0100

    Replace tabs in CMake files with spaces

--- cmake/Modules/FindLibmicrohttpd.cmake
+++ cmake/Modules/FindLibmicrohttpd.cmake
@@ -9,19 +9,19 @@ find_package(Threads REQUIRED)
 find_package(PkgConfig QUIET)
 
 if(PKG_CONFIG_FOUND)
-	pkg_check_modules(PC_LIBMICROHTTPD QUIET libmicrohttpd)
+    pkg_check_modules(PC_LIBMICROHTTPD QUIET libmicrohttpd)
 endif()
 
 if(Libmicrohttpd_USE_STATIC_LIBS)
-	set(Libmicrohttpd_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
+    set(Libmicrohttpd_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
     set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
 endif()
 
 find_path(Libmicrohttpd_INCLUDE_DIR microhttpd.h
-	HINTS ${PC_LIBMICROHTTPD_INCLUDEDIR} ${PC_LIBMICROHTTPD_INCLUDE_DIRS})
+    HINTS ${PC_LIBMICROHTTPD_INCLUDEDIR} ${PC_LIBMICROHTTPD_INCLUDE_DIRS})
 
 find_library(Libmicrohttpd_LIBRARY NAMES microhttpd
-	HINTS ${PC_LIBMICROHTTPD_LIBDIR} ${PC_LIBMICROHTTPD_LIBRARY_DIRS})
+    HINTS ${PC_LIBMICROHTTPD_LIBDIR} ${PC_LIBMICROHTTPD_LIBRARY_DIRS})
 
 if(Libmicrohttpd_USE_STATIC_LIBS)
     set(CMAKE_FIND_LIBRARY_SUFFIXES ${Libmicrohttpd_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES})
@@ -31,7 +31,7 @@ include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set Libmicrohttpd_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(Libmicrohttpd DEFAULT_MSG
-	Libmicrohttpd_LIBRARY Libmicrohttpd_INCLUDE_DIR)
+    Libmicrohttpd_LIBRARY Libmicrohttpd_INCLUDE_DIR)
 
 mark_as_advanced(Libmicrohttpd_INCLUDE_DIR Libmicrohttpd_LIBRARY)
 
@@ -39,14 +39,14 @@ set(Libmicrohttpd_LIBRARIES ${Libmicrohttpd_LIBRARY} ${CMAKE_THREAD_LIBS_INIT})
 set(Libmicrohttpd_INCLUDE_DIRS ${Libmicrohttpd_INCLUDE_DIR})
 
 if (Libmicrohttpd_FOUND AND NOT TARGET Libmicrohttpd)
-	add_library(Libmicrohttpd UNKNOWN IMPORTED)
+    add_library(Libmicrohttpd UNKNOWN IMPORTED)
 
-	set_target_properties(Libmicrohttpd PROPERTIES
+    set_target_properties(Libmicrohttpd PROPERTIES
         IMPORTED_LINK_INTERFACE_LANGUAGES "C"
         IMPORTED_LOCATION "${Libmicrohttpd_LIBRARY}"
         INTERFACE_INCLUDE_DIRECTORIES "${Libmicrohttpd_INCLUDE_DIRS}"
-		INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${Libmicrohttpd_INCLUDE_DIRS}"
-		INTERFACE_LINK_LIBRARIES "${Libmicrohttpd_LIBRARIES}"
-		#INTERFACE_COMPILE_OPTIONS "${Libmicrohttpd_DEFINITIONS}"
+        INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${Libmicrohttpd_INCLUDE_DIRS}"
+        INTERFACE_LINK_LIBRARIES "${Libmicrohttpd_LIBRARIES}"
+        #INTERFACE_COMPILE_OPTIONS "${Libmicrohttpd_DEFINITIONS}"
     )
 endif()
--- cmake/Modules/FindSystemd.cmake
+++ cmake/Modules/FindSystemd.cmake
@@ -10,35 +10,35 @@
 find_package(PkgConfig QUIET)
 
 if(PKG_CONFIG_FOUND)
-	pkg_check_modules(PC_SYSTEMD QUIET libsystemd)
+    pkg_check_modules(PC_SYSTEMD QUIET libsystemd)
 endif()
 
 # Probably, there is no static library of systemd
 #if(Systemd_USE_STATIC_LIBS)
-#	set(Systemd_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
-#	set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
+#    set(Systemd_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
+#    set(CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_STATIC_LIBRARY_SUFFIX})
 #endif()
 
 if(PC_SYSTEMD_FOUND)
-	foreach(item ${PC_SYSTEMD_CFLAGS})
-		if("${item}" MATCHES "-D.*")
-			list(APPEND Systemd_DEFINITIONS ${item})
-		endif()
-	endforeach()
+    foreach(item ${PC_SYSTEMD_CFLAGS})
+        if("${item}" MATCHES "-D.*")
+            list(APPEND Systemd_DEFINITIONS ${item})
+        endif()
+    endforeach()
 else()
-	if(Systemd_CUSTOM_DEFINITIONS)
-		set(Systemd_DEFINITIONS ${Systemd_CUSTOM_DEFINITIONS})
+    if(Systemd_CUSTOM_DEFINITIONS)
+        set(Systemd_DEFINITIONS ${Systemd_CUSTOM_DEFINITIONS})
     endif()
 endif()
 
 find_path(Systemd_INCLUDE_DIR systemd/sd-login.h
-	HINTS ${PC_SYSTEMD_INCLUDEDIR} ${PC_SYSTEMD_INCLUDE_DIRS})
+    HINTS ${PC_SYSTEMD_INCLUDEDIR} ${PC_SYSTEMD_INCLUDE_DIRS})
 
 find_library(Systemd_LIBRARY NAMES systemd
-	HINTS ${PC_SYSTEMD_LIBDIR} ${PC_SYSTEMD_LIBRARY_DIRS})
+    HINTS ${PC_SYSTEMD_LIBDIR} ${PC_SYSTEMD_LIBRARY_DIRS})
 
 #if(Systemd_USE_STATIC_LIBS)
-#	set(CMAKE_FIND_LIBRARY_SUFFIXES ${Systemd_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES})
+#    set(CMAKE_FIND_LIBRARY_SUFFIXES ${Systemd_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES})
 #endif()
 
 set(Systemd_LIBRARIES ${Systemd_LIBRARY})
@@ -49,18 +49,18 @@ include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set Systemd_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(Systemd DEFAULT_MSG
-	Systemd_LIBRARY Systemd_INCLUDE_DIR)
+    Systemd_LIBRARY Systemd_INCLUDE_DIR)
 mark_as_advanced(Systemd_INCLUDE_DIR Systemd_LIBRARY)
 
 if (Systemd_FOUND AND NOT TARGET Systemd::Systemd)
-	add_library(Systemd::Systemd UNKNOWN IMPORTED)
+    add_library(Systemd::Systemd UNKNOWN IMPORTED)
 
-	set_target_properties(Systemd::Systemd PROPERTIES
+    set_target_properties(Systemd::Systemd PROPERTIES
         IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-		IMPORTED_LOCATION "${Systemd_LIBRARY}"
-		INTERFACE_INCLUDE_DIRECTORIES "${Systemd_INCLUDE_DIRS}"
-		INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${Systemd_INCLUDE_DIRS}"
-		INTERFACE_LINK_LIBRARIES "${Systemd_LIBRARIES}"
-		INTERFACE_COMPILE_OPTIONS "${Systemd_DEFINITIONS}"
+        IMPORTED_LOCATION "${Systemd_LIBRARY}"
+        INTERFACE_INCLUDE_DIRECTORIES "${Systemd_INCLUDE_DIRS}"
+        INTERFACE_SYSTEM_INCLUDE_DIRECTORIES "${Systemd_INCLUDE_DIRS}"
+        INTERFACE_LINK_LIBRARIES "${Systemd_LIBRARIES}"
+        INTERFACE_COMPILE_OPTIONS "${Systemd_DEFINITIONS}"
     )
 endif()
